bc43949f703e575af7972e62a252c5af71e7efd0
sumaiya-rahman-chowdhury <bdfara@1>
2025-01-03 23:30:29 +0600

    setup tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-red-800">
+      Hello world!
+    </h1>
+    </div>
+  )
+}
